ad34130a23ba119ccee0d77ed833e5c2c90d0639
Soohyun-CHOI <csh0sns@naver.com>
2020-01-21 15:06:20 +0900

    finish

---
+++ core/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from core.models import *
+
+admin.site.register(Customer)
+admin.site.register(Order)
+admin.site.register(Post)
+admin.site.register(Comment)
+admin.site.register(Profile)
+
